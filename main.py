5dad7676be9efee65bb63c19a08f0d8155e5d74d
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2024-04-06 20:00:59 +0530

    feat: add decrypt code

--- main.py
+++ main.py
@@ -18,11 +18,27 @@ def encrypt(msg, n):
             e_msg+=chr(val % 26 + 97)
     print("\n"+"Encrypted text: "+e_msg+"\n")
 
+def decrypt(msg, n):
+    e_msg=""
+    length=len(msg)
+    for i in range(length):
+        ch=msg[i]
+        if ch==" ":
+            e_msg+=" "
+        elif(ch.isupper()):
+            val=ord(ch) - n - 65
+            e_msg+=chr( val % 26 + 65)
+        elif(ch.islower()):
+            val=ord(ch) - n - 97
+            e_msg+=chr(val % 26 + 97)
+    print("\n"+"Decrypted text: "+e_msg+"\n")
 
 def main():
     str=input("enter text: ")
-    n=int(input("enter shift value to encrypt: "))
+    n=int(input("enter shift value : "))
+    
     encrypt(str,n)
+    decrypt(str,n)
 
 
 if __name__=="__main__":
