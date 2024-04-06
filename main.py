f8892f5ccbc86b800824502828d0aa609c09edaf
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2024-04-06 20:11:05 +0530

    feat: add menu

--- main.py
+++ main.py
@@ -36,9 +36,19 @@ def decrypt(msg, n):
 def main():
     str=input("enter text: ")
     n=int(input("enter shift value : "))
-    
-    encrypt(str,n)
-    decrypt(str,n)
+    while True:
+        print("\n1.Encrypt")
+        print("\n2.Decrypt")
+        print("\n3.Exit")
+        ch=int(input("enter your choice: "))
+        if ch==1:
+            encrypt(str,n)
+        elif ch==2:
+            decrypt(str,n)
+        elif ch==3:
+            break
+        else:
+            print("\nwrong choice")
 
 
 if __name__=="__main__":
