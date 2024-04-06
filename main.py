9af0765d9d6670843da53d066e814a2213440aa0
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2024-04-06 19:55:43 +0530

    feat: add encrypt code

---
+++ main.py
@@ -0,0 +1,29 @@
+#encrypt/decrypt text Caesar Cipher algorithm
+#user input a message
+#input shift value
+#encrypt/decrypt
+
+def encrypt(msg, n):
+    e_msg=""
+    length=len(msg)
+    for i in range(length):
+        ch=msg[i]
+        if ch==" ":
+            e_msg+=" "
+        elif(ch.isupper()):
+            val=ord(ch) + n - 65
+            e_msg+=chr( val % 26 + 65)
+        elif(ch.islower()):
+            val=ord(ch) + n - 97
+            e_msg+=chr(val % 26 + 97)
+    print("\n"+"Encrypted text: "+e_msg+"\n")
+
+
+def main():
+    str=input("enter text: ")
+    n=int(input("enter shift value to encrypt: "))
+    encrypt(str,n)
+
+
+if __name__=="__main__":
+    main()
